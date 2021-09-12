eea1944c07cda41e1ef4042e9847df8a8801a1e7
uc-day2jd <day2jd@mail.uc.edu>
2021-09-12 15:15:39 -0400

    powershell files

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -3,3 +3,28 @@
 (Get-NetIPAddress).ipv4address | Select-String "192*"
 
 }
+
+Function localuser{
+
+Get-localUser | select-string "Adm*"
+
+}
+
+Function hostname{
+
+hostname
+}
+
+Function version{
+
+$Host.Version.Major
+
+}
+
+Function body{
+
+}
+
+Function todaysdate{
+get-date
+}
